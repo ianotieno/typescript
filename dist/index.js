939eb957cd85f2f4187fe1e8241ea26db167d573
ian otieno <ianotieno23@gmail.com>
2023-09-11 23:18:47 +0300

    update

---
+++ dist/index.js
@@ -0,0 +1,27 @@
+"use strict";
+let ida = 5;
+let company = 'ian';
+let itscomplicated = true;
+let x = 'hello';
+let id = [1, 2, 3, 4, 5];
+id.push(2);
+let person = [1, 'tom', true];
+let employee;
+employee = [
+    [1, 'ian'], [2, 'paul']
+];
+let ids;
+ids = '22';
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 2] = "Up";
+    Direction1[Direction1["Down"] = 3] = "Down";
+    Direction1[Direction1["Left"] = 4] = "Left";
+    Direction1[Direction1["Right"] = 5] = "Right";
+})(Direction1 || (Direction1 = {}));
+console.log(Direction1.Right);
+const user = {
+    id: 1,
+    name: 'ian'
+};
+console.log(user);
---
+++ src/index.ts
@@ -0,0 +1,33 @@
+let ida: number=5
+let company:string ='ian'
+let itscomplicated:boolean=true
+let x :any ='hello'
+
+let id :number[]= [1,2,3,4,5]
+id.push(2)
+let person:[number,string,boolean]=[1,'tom',true]
+let employee:[number,string][]
+employee=[
+[ 1,'ian'],[2,'paul']]
+
+let ids: string | number 
+ids='22'
+
+enum Direction1{
+    Up=2,
+    Down,
+    Left,
+    Right
+}
+
+console.log(Direction1.Right)
+
+type User={
+    id:number,
+    name: string
+}
+const user:User={
+    id:1,
+    name:'ian'
+}
+console.log(user)
